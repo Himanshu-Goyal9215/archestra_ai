310b6b5c913cb8c20ab0a560a05cfc4c9a1765f4
Himanshu-Goyal9215 <goyalhimanshu096@gmail.com>
2026-02-17 10:42:36 +0530

    feat: the firebase configs were hardcoded , now they are added in .env for security reasons

--- src/app/api/health-chat/route.ts
+++ src/app/api/health-chat/route.ts
@@ -3,13 +3,13 @@ import { initializeApp, getApps } from "firebase/app";
 import { getFirestore, collection, query, where, getDocs, orderBy } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAL7PJQF2LmLHfU6IY2HO5QN7es44qH9lE",
-    authDomain: "personal-assistant-85a0e.firebaseapp.com",
-    projectId: "personal-assistant-85a0e",
-    storageBucket: "personal-assistant-85a0e.firebasestorage.app",
-    messagingSenderId: "568287775887",
-    appId: "1:568287775887:web:8e1647d7fd0bda8e6db4fa",
-    measurementId: "G-1LWZPQ72J8",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
